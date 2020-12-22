720a9ff2f1c64a535f1f381c1f3ac2f52be380df
dennis.mullen <dennis.mullen@dennis.mullen>
2020-12-21 19:47:58 -0500

    style pw reset form

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/dkm/code/semantic/semantic-ui/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/dkm/code/semantic/semantic-ui/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/dennis.mullen/Documents/movie-db/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/dennis.mullen/Documents/movie-db/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
